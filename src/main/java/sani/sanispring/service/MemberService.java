a8bacef37bf5a13e7691bb11ba8335305b077c80
sungsan <kunga077@gmail.com>
2020-11-17 01:45:58 +0900

    회원관리 MVC개발 간단한 GET과 POST 요청에 대한 응답 처리

--- src/main/java/sani/sanispring/service/MemberService.java
+++ src/main/java/sani/sanispring/service/MemberService.java
@@ -28,9 +28,12 @@ public class MemberService {
           throw new IllegalStateException("이미 존재하는 회원");
         });
   }
+
   public List<Member> findMembers() {
+
     return memberRepository.findAll();
   }
+
   public Optional<Member> findOne(Long memberId) {
     return memberRepository.findById(memberId);
   }
